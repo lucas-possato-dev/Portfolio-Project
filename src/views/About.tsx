4f134b51820c2f7a29ed9e2797bb30c5ed7d3dc9
lucas-possato-dev <lucaspstinho@gmail.com>
2023-09-20 11:40:27 -0300

    main sessions finished

---
+++ src/views/About.tsx
@@ -0,0 +1,62 @@
+// assets
+import aboutPageImg from "../assets/about-me-page.svg";
+import aboutIllustration from "../assets/about-illustration.svg";
+import facebookIcon from "../assets/facebook-icon.svg";
+import instagramIcon from "../assets/instagram-icon.svg";
+import twitterIcon from "../assets/twitter-icon.svg";
+import youtubeIcon from "../assets/youtube-icon.svg";
+
+// components
+import { SocialMediaIcon, Reveal } from "../components";
+
+// framer-motion
+import { motion } from "framer-motion";
+
+// utils
+import { fadeIn, scale } from "../utils/variants";
+import { transition } from "../utils/transition";
+
+const About = () => {
+  return (
+    <div
+      id="about"
+      className="min-h-screen flex items-center justify-center relative"
+      style={{
+        background: `url(${aboutPageImg})`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="max-w-screen-2xl flex flex-col xl:flex-row xl:justify-between items-center xl:items-start gap-12 w-full py-16 px-12">
+        <div className="flex flex-1 flex-col gap-4">
+          <h2 className="text-center xl:text-start text-4xl sm:text-5xl lg:text-[64px] font-bold text-textPrimary ">
+            About <span className="text-secondary">me</span>
+          </h2>
+          <p className="text-center xl:text-start text-base sm:text-lg text-textSecondary">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem nam
+            iusto, est atque rerum placeat? Perferendis quibusdam, sed nemo,
+            eveniet voluptatum repellendus velit deleniti et aut quia in dolorum
+            ducimus.
+          </p>
+          <div className="flex items-center justify-center xl:justify-start gap-6">
+            <SocialMediaIcon imgSrc={facebookIcon} title="Facebook" />
+            <SocialMediaIcon imgSrc={instagramIcon} title="Instagram" />
+            <SocialMediaIcon imgSrc={twitterIcon} title="Twitter" />
+            <SocialMediaIcon imgSrc={youtubeIcon} title="Youtube" />
+          </div>
+        </div>
+        <div className="flex-1 flex items-center justify-center">
+          <img
+            src={aboutIllustration}
+            className="max-w-full sm:max-w-[401px]"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="absolute bottom-0 left-0 w-full h-[1px] bg-divider" />
+    </div>
+  );
+};
+
+export default About;
---
+++ src/views/Projects.tsx
@@ -0,0 +1,77 @@
+// react
+import { useState } from "react";
+
+// assets
+import projectsPageImg from "../assets/projects-page.svg";
+
+// components
+import { Button, Card, Reveal } from "../components";
+
+// data
+import { projects } from "../data";
+
+// framer-motion
+import { motion } from "framer-motion";
+
+// utils
+import { fadeIn } from "../utils/variants";
+import { transition } from "../utils/transition";
+
+type Category = "uiUx" | "web";
+
+const Projects = () => {
+  const [activeCategory, setActiveCategory] = useState<Category>("uiUx");
+
+  const filterProjects = () => {
+    if (activeCategory === "uiUx") {
+      return projects.filter((item) => item.category === "uiUx");
+    } else {
+      return projects.filter((item) => item.category === "web");
+    }
+  };
+
+  return (
+    <div
+      id="project"
+      className="min-h-screen relative"
+      style={{
+        background: `url(${projectsPageImg})`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="max-w-screen-2xl w-full py-16 px-12 mx-auto">
+        <div className="flex-1 flex flex-col gap-4">
+          <h2 className="text-center xl:text-start text-4xl sm:text-5xl lg:text-[64px] font-bold text-textPrimary ">
+            My recent <span className="text-secondary">projects</span>
+          </h2>
+
+          <div className="flex items-center gap-4 justify-center xl:justify-start flex-col sm:flex-row">
+            <Button
+              secondary={activeCategory === "uiUx" ? true : false}
+              onClick={() => setActiveCategory("uiUx")}
+            >
+              UI/UX
+            </Button>
+            <Button
+              secondary={activeCategory === "web" ? true : false}
+              onClick={() => setActiveCategory("web")}
+            >
+              Web Design
+            </Button>
+          </div>
+
+          <div className="flex gap-12 mt-12 flex-wrap justify-center">
+            {filterProjects().map((item) => (
+              <Card imgSrc={item.img} title={item.title} />
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="absolute bottom-0 left-0 w-full h-[1px] bg-divider" />
+    </div>
+  );
+};
+
+export default Projects;
